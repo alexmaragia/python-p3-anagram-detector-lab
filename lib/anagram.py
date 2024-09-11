9c22749fada677abb9d853150f682fefdd5533d1
alexmaragia <maragialex@gmail.com>
2024-09-11 13:14:57 +0300

    python-p3-anagram-detector-lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, word_list):
+        matches = []
+        for word in word_list:
+            if self._is_anagram(word):
+                matches.append(word)
+        return matches
+
+    def _is_anagram(self, other_word):
+        if self.word.lower() == other_word.lower():
+            return False
+        return sorted(other_word.lower()) == self.sorted_word
